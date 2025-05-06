72e69a79b930a236195b46816cffc869039ef7f8
LUIZ EMANUEL COLLARES MASCARENHAS <luizmascarenhas9380@digitalcollege.internal>
2025-05-06 12:03:28 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+window.onload = () => {
+    carregarVagas();
+}
+
+async function carregarVagas () {
+    try {
+        const { data : vagas } = await axios.get('http://localhost:3000/jobs');
+        mostrarVagas(vagas);
+    } catch (error) {
+        console.log(error);
+        
+        mostrarErro('Erro ao carregar Vagas')
+
+    }
+}
+function mostrarVagas(vagas) {
+    const container = document.getElementById('job-list');
+    container.innerHTML = vagas.map(vaga => 
+        `
+    <div class="col-md-4 mb-4">
+      <div class="card border-info shadow-sm">
+        <div class="card-body">
+          <h5 class="card-title">${vaga.title}</h5>
+          <p class="card-text"><strong>Empresa:</strong> ${vaga.company}</p>
+          <p class="card-text">
+            <span class="badge rounded-pill bg-info text-muted">
+              ${vaga.location || 'Não informado'}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+        
+        `
+    ).join('');
+}
+
+function carregarCandidatos() {
+  axios.get('http://localhost:3000/user')
+  
+}
+function mostrarErro(msg) {
+    Swal.fire('Error', msg, "error")
+}
